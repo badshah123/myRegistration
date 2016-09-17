6397eceedfb8fd06ff625acfd228bd1b9d50ce75
badshah123 <ibrahimbadushah123@gmail.com>
2016-09-17 12:30:46 +0530

    registration commit

---
+++ app/src/main/java/com/example/student/myregistration/Register.java
@@ -0,0 +1,17 @@
+package com.example.student.myregistration;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Student on 17-09-2016.
+ */
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
